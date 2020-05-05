b129ca07f170a264a6ae5039e4d90d70b46294b0
yusoojeong <yusj1231@gmail.com>
2020-05-05 15:33:32 +0000

    200506 | added signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def signup(request):
+
+    form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/signup.html', context)
