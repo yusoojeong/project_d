e1453299af8bea3c28b8ae9ea6d62c6b9cbbdef4
yusoojeong <yusj1231@gmail.com>
2020-05-05 15:58:10 +0000

    200506 | diary migrate

--- diary/models.py
+++ diary/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contirb.auth import settings
+from django.contrib.auth import settings
 from django.utils import timezone
 
 # Create your models here.
@@ -7,6 +7,6 @@ class Post(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
     # 현재의 시간 값을 디폴트값으로 받아가는 date
-    date = models.TimestampField(default=timezone.now)
+    date = models.DateTimeField(default=timezone.now)
     users = models.ForeignKey(settings.AUTH_USER_MODEL,
                             on_delete=models.CASCADE)
--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    pass
