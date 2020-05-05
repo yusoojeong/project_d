cd53b49a597501085f8f1415eb40d7b02e157ec0
yusoojeong <yusj1231@gmail.com>
2020-05-05 16:05:35 +0000

    200506 | added diary create

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
-    pass
+    posts = Post.objects.order_by('-pk')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'diary/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return render('diary:index')
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'diary/form.html', context)
